935526e6a7080d72d022eecdb8f4211398907c81
Steven Barnhurst <s.barnhurst@xeotek.com>
2024-12-27 12:26:58 +0100

    Adds initial component structure

---
+++ starter/src/shared/Book.js
@@ -0,0 +1,32 @@
+export const Book = ({book}) => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage:
+              `url("${book.imageLinks.thumbnail}")`,
+          }}
+        ></div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">
+              Currently Reading
+            </option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">1776</div>
+      <div className="book-authors">David McCullough</div>
+    </div>
+  )
+}
