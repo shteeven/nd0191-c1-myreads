9839d27244773783faef53ef15d39ccc56c4e6a3
Steven Barnhurst <s.barnhurst@xeotek.com>
2024-12-30 18:37:02 +0100

    Adds 'no results' display for BookShelf component

--- starter/src/book-shelf-page/BookShelf.tsx
+++ starter/src/book-shelf-page/BookShelf.tsx
@@ -10,13 +10,16 @@ class BookShelfProps {
 
 export const BookShelf = ({books, config, onUpdateShelf}: BookShelfProps) => {
   const bookOnShelf = books?.filter((book) => book.shelf === config.shelfKey);
+
+  const bookShelfResults = !bookOnShelf?.length ? <p>No books on this shelf, yet.</p> : <ol className="books-grid">
+    {bookOnShelf.map((book) => (<li key={book.id}><Book book={book} onUpdateShelf={onUpdateShelf}/></li>))}
+  </ol>
+
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{config.title}</h2>
       <div className="bookshelf-books">
-        <ol className="books-grid">
-          {bookOnShelf.map((book) => (<li key={book.id}><Book book={book} onUpdateShelf={onUpdateShelf}/></li>))}
-        </ol>
+        {bookShelfResults}
       </div>
     </div>
   )
